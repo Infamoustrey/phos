7ec1124ed9b308dfcc0510c12be07443fbc1c7f8
Trey Foster <treybfoster@gmail.com>
2019-11-16 16:18:41 -0600

    add stronger type definitions for textitems

---
+++ app/interfaces/TextItem.ts
@@ -0,0 +1,8 @@
+export interface TextItem {
+  id: string;
+  service_item_id: string;
+  text: string;
+  options: TextItemOptions;
+}
+
+export interface TextItemOptions {}
--- app/store/TextItems.ts
+++ app/store/TextItems.ts
@@ -1,4 +1,5 @@
 import * as uuid from "uuid/v1";
+import { TextItem } from "../interfaces/TextItem";
 
 const ADD_TEXT_ITEM = "ADD_TEXT_ITEM";
 const CREATE_TEXT_ITEM = "CREATE_TEXT_ITEM";
@@ -7,16 +8,22 @@ export { ADD_TEXT_ITEM, CREATE_TEXT_ITEM };
 
 export default {
   state: {
-    textItems: []
+    textItems: [] as Array<TextItem>
   },
   reducers: {
-    [ADD_TEXT_ITEM]: (global, dispatch, textItem) => ({
+    [ADD_TEXT_ITEM]: (global, dispatch, textItem: TextItem) => ({
       textItems: [...global.textItems, textItem]
     })
   },
   actions: {
-    [CREATE_TEXT_ITEM]: async (global, dispatch, item_id, text, options) => {
-      let textItem = { id: uuid(), item_id, text, options };
+    [CREATE_TEXT_ITEM]: async (
+      global,
+      dispatch,
+      service_item_id,
+      text,
+      options
+    ) => {
+      let textItem: TextItem = { id: uuid(), service_item_id, text, options };
 
       await dispatch[ADD_TEXT_ITEM](textItem);
 
